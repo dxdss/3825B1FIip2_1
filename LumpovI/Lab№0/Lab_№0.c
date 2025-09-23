78cc0ca08f2d1171bba75f0b1166da7b9c85cbcf
unknown <lumpov.i.74@mail.ru>
2025-09-23 16:51:15 +0300

    Lab0_edited2

--- LumpovI/Lab№0/Lab_№0.c
+++ LumpovI/Lab№0/Lab_№0.c
@@ -4,12 +4,13 @@
 
 int main() {
 
-	double r1, r2, x1, y1, x2, y2 = 0;
+	double r1 = 0, r2 = 0, x1 = 0, y1 = 0, x2 = 0 , y2 = 0;
 	while (scanf("%lf%lf%lf%lf%lf%lf", &r1, &r2, &x1, &y1, &x2, &y2) != 6 || r1 < 0 || r2 < 0) {
 		printf("Incorrect input!!!");
 		return -1;
 	}
-	double rast = sqrt(((x1 - x2) * (x1 - x2) - (y1 - y2) * (y1 - y2)), 0.5);
+	double xy = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+	double rast = pow(xy, 0.5);
 	double sum_rad = r1 + r2;
 	int x = 0;
 	if (rast < r1 || rast < r2) {
@@ -32,6 +33,9 @@ int main() {
 			else if (rast > sum_rad) {
 				printf("Circles externally not intersect");
 			}
+			else if (rast == sum_rad) {
+				printf("Circles externally touch");
+			}
 			break;
 		case(2):
 			if (rast == fabs(r1 - r2)) {
@@ -43,10 +47,6 @@ int main() {
 			else if (rast > fabs(r1 - r2)) {
 				printf("Circles intersect internally");
 			}
-			break;
-		default:
-			printf("Error!!!");
-			return -1;
 			break;
 		}
 	}
