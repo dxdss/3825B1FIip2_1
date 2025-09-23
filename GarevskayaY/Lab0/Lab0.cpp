2269c777f7ead7f4250a90038e9b2e45e71f9188
unknown <yarrreeek@ya.ru>
2025-09-23 21:49:15 +0300

    add_files

--- GarevskayaY/Lab0/Lab0.cpp
+++ GarevskayaY/Lab0/Lab0.cpp
@@ -4,24 +4,42 @@
 
 int main() {
     double x1, y1, x2, y2, r1, r2 = 0;
+    double EPS = 1e-6;
+
     printf("X1 Y1: ");
     scanf("%lf%lf", &x1, &y1);
+  
     printf("X2 Y2: ");
     scanf("%lf%lf", &x2, &y2);
+
     printf("R1 R2: ");
     scanf("%lf%lf", &r1, &r2);
-    double xx = pow(x1 - x2, 2);
-    double yy = pow(y1 - y2, 2);
-    double d = pow(xx + yy, 0.5);
-    if (d > r1 + r2) {
-        printf("Okruzhnosti ne peresekayutsa");
+    while (r1 < 0 - EPS || r2 < 0 - EPS ) {
+        printf("Error, enter again:\t");
+        scanf("%lf%lf", &r1, &r2);
     }
-    else if (d < r1 + r2) {
-        printf("Okruzhnosti peresekayutsa");
+    double xx = (x1 - x2)*(x1 - x2);
+    double yy = (y1 - y2)*(y1 - y2);
+    double d = sqrt((xx + yy));
+
+    if (d == 0) {
+        printf("The centers of the circles coincide"); // Центры совпадают
     }
-    else if (d = r1 + r2) {
-        printf("Okruzhnosti kasayutsa");
+    else if (d > r1 + r2) {
+        printf("The circles don't intersect"); // Не пересекаются
     }
- 
+    else if (r1 - r2 < d < r1 + r2) {
+        printf("The circles intersect"); // Пересекаются
+    }
+    else if (d == r1 + r2) {
+        printf("The circles externally touch"); // Касаются внешне
+    }
+    else if (d == r1 - r2 && d > 0) {
+        printf("The circles are internally tangent"); // Касаются внутренне
+    }
+    else if (d < r1 - r2) {
+        printf("One circle lies inside the other"); // Одна лежит внутри другой
+    }
+    
 }
 
