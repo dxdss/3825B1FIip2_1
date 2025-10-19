c70cbf39d542d385e7d801b1be91ce6dbdc48dbe
unknown <tvink3359@gmail.com>
2025-10-19 22:21:28 +0300

    lab3_fix2

--- BespalovR/Lab№3/Lab№3.c
+++ BespalovR/Lab№3/Lab№3.c
@@ -12,23 +12,20 @@ void main() {
 	char c;
 	char lngth;
 	printf("Input length of guessed number.(2-5)\n");
-	while (scanf("%c", &lngth) != 1 || lngth - '0' > 5 || lngth - '0' < 2) {
+	while (scanf("%c", &lngth) != 1 || ( lngth -= '0' ) > 5 || lngth  < 2) {
 		printf("Incorrect input. Please try again.\n");
 		while ((c = getchar()) != '\n') {};
 	}
 
-
 	//Creating random 2-5 digit array with unique numbers.
+	char t;
 	char i = 0;
-	char t = 0;
 	char array[5];
 	char num[10] = { 0 };
 	srand(time(NULL));
-	while (i < lngth - '0') {
-		t = rand() % 10;
-		if (num[t] == 0 && (t > 0 || i > 0)) {
-			array[i] = t;
-			num[array[i++]] += 1;
+	while (i < lngth ) {
+		if (num[t = rand() % 10] == 0 && (t > 0 || i > 0)) {
+			num[array[i++] = t] += 1;
 		}
 	}
 
@@ -44,24 +41,24 @@ void main() {
 		while (i != 0) {
 			//There's some strange behaviour of endlines with getchar, dunno how to fix it
 
-			if ((c = getchar()) > '9' || c < '0') {
+			if ((c = getchar() - '0') > 9 || c < 0) {
 				printf("Error.\n");
 				break;
 			}
 
-			else if (c - '0' == array[lngth - '0' - i]) {
+			else if (c  == array[lngth - i]) {
 				++cows;
 			}
 
-			else if (num[c - '0'] != 0) {
+			else if (num[c] != 0) {
 				++t;
 			}
 
 			--i;
 		}
 		printf("There is %d cows and %d oxen.\n", cows, t);
-		i = lngth - '0';
-	} while (cows != lngth - '0');
+		i = lngth;
+	} while (cows != lngth );
 
 	printf("You Won!\n");
 }
