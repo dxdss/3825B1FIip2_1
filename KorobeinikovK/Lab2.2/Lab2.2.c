f58de1b919fc4dbef38903e71ad67f586af7c487
unknown <k.kirill.a190208@gmail.com>
2025-10-19 22:04:49 +0300

    Lab2try2

---
+++ KorobeinikovK/Lab2.2/Lab2.2.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define OUT 0
+#define WORD 1
+#define NUM 2
+
+int main() {
+	char c;
+	int state, nw = 0, nn = 0;
+	state = OUT;
+	int letter, digit;
+	while ((c = getchar()) != '\n') {
+		letter = (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+		digit = (c >= '0' && c <= '9');
+		if (c == ' ' || c == '\t') {
+			state = OUT;
+		}
+		else if (letter) {
+			if (state != WORD) {
+				state = WORD;
+				++nw;
+			}
+		}
+		else if (digit) {
+			if (state != NUM) {
+				state = NUM;
+				++nn;
+			}
+		}
+		else {
+			state = OUT;
+		}
+	}
+	printf("Result\n");
+	printf("Word count: %d\n", nw);
+	printf("Number count: %d\n", nn);
+}
